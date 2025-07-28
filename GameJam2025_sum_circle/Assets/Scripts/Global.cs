6704d100d7ab7ac00ac07a7f4b010fb054976682
star <KurosawaReo.Job@gmail.com>
2025-07-28 15:28:31 +0900

    Unityプロジェクトの用意

---
+++ GameJam2025_sum_circle/Assets/Scripts/Global.cs
@@ -0,0 +1,40 @@
+
+/// <summary>
+/// グローバルのテンプレ(空)
+/// 汎用定数や汎用関数などをまとめる用.
+/// </summary>
+namespace Global
+{
+
+
+    /// <summary>
+    /// Global定数.
+    /// </summary>
+    public static class GL_Const
+    {
+#if false
+        //例:
+        public const int COUNT = 3;
+        
+        public static string[] TEXT =
+        {
+            "AAA",
+            "BBB",
+            "CCC",
+        };
+#endif
+    }
+
+    /// <summary>
+    /// Global関数.
+    /// </summary>
+    public static class GL_Func
+    {
+#if false
+        public static void Test()
+        {
+
+        }
+#endif
+    }
+}
